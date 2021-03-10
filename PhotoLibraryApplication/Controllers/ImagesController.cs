741e8232f86b81df2507f2a85930d9a88dc67ce6
Aaron van den Berg <a.vdberg98@gmail.com>
2021-03-10 15:45:20 +0100

    Image size is now "Tiny"

--- PhotoLibraryApplication/Controllers/ImagesController.cs
+++ PhotoLibraryApplication/Controllers/ImagesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -35,7 +36,7 @@ namespace PhotoLibraryApplication.Controllers
                 return result.photos.Select(photo => new Image
                     {
                         Id = photo.id,
-                        Url = photo.source.original,
+                        Url = photo.source.tiny,
                         Width = photo.width,
                         Height = photo.height,
                         Photographer = photo.photographer
